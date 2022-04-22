05b164f1210ce78e6eec7f5e87ed32a1112d7621
Irhin <a.irhin@gk-adept.ru>
2022-04-23 01:00:23 +0300

    fix bugs

--- src/components/Format/index.tsx
+++ src/components/Format/index.tsx
@@ -41,30 +41,30 @@ const data: Record<string, any> = {
       third: "third",
       first: [1],
     },
-	{
-		first: [1],
-		second: [2],
-		third: [3],
-		arrayWithObj: [
-		  {
-			first: "first",
-			second: [2],
-		  },
-		  {
-			second: "second",
-			third: [3],
-		  },
-		  {
-			third: "third",
-			first: [1],
-		  },
-		],
-	  },
+    {
+      first: [1],
+      second: [2],
+      third: [3],
+      arrayWithObj: [
+        {
+          first: "first",
+          second: [2],
+        },
+        {
+          second: "second",
+          third: [3],
+        },
+        {
+          third: "third",
+          first: [1],
+        },
+      ],
+    },
   ],
 };
 
 const Format = (): JSX.Element => {
-  return <textarea value={JSON.stringify(upperCaseKeys(data), null, 2)} />;
+  return <pre>{JSON.stringify(upperCaseKeys(data), undefined, 2)}</pre>;
 };
 
 export default Format;
